e2c922e7d1351ddf9a2a0944ec3b91e6b48c751d
Radoslav Figura <radoslav.figura@student.tuke.sk>
2019-03-21 23:15:05 +0100

    Added new icon fa-question

--- src/components/faLib.js
+++ src/components/faLib.js
@@ -1,8 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-
-import { faSearch, faArrowRight} from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faArrowRight, faQuestion} from '@fortawesome/free-solid-svg-icons';
 
 library.add(
     faSearch,
-    faArrowRight
+    faArrowRight,
+    faQuestion
 );
